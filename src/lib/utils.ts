36a9421e63619ab209e2e99e2e81200b6de79a0b
zsfer <seiferalbacete@gmail.com>
2024-11-24 00:57:49 +0800

    fix: isValidCarehub

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,16 +8,4 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const isValidCarehubRecord = (record?: CarehubRecord) =>
-  record && dayjs(record.dateRecorded).isSame(new Date(), "day");
-
-export const toAirtableSafeCarehub = (record: CarehubRecord) => {
-  return {
-    ID: record.id,
-    "Date Recorded": record.dateRecorded,
-    "Additional Info": record.additionalInfo,
-    Status: record.status,
-    "Health Check": record.healthCheck,
-    "Mental Health Check": record.mentalHealthCheck,
-    Student: record.student,
-  };
-};
+  record && dayjs(record["Date Recorded"]).isSame(new Date(), "day");
